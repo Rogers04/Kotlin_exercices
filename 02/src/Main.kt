4289a2cce2f9b47fe24ee3eed2832ee224acdc40
Rogers Lucas <138635451+Rogers04@users.noreply.github.com>
2024-08-31 21:06:10 -0300

    02
    Verificação para ver se os números são pares ou ímpares

---
+++ 02/src/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val InputNum = readLine()!!.toInt()!!
+
+    if (InputNum % 2 == 0 ){
+        println("O número $InputNum é par")
+    }
+    else {
+        println("O número $InputNum é ímpar")
+    }
+
+}
